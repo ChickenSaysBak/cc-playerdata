705be0f86e9476ae25b5de48c0527ff85bef6b17
ChickenSaysBak <nicholash2000@icloud.com>
2023-07-08 23:39:19 -0400

    Added versatile fromString method

--- src/main/java/me/chickensaysbak/ccplayerdata/model/Rank.java
+++ src/main/java/me/chickensaysbak/ccplayerdata/model/Rank.java
@@ -1,5 +1,22 @@
 package me.chickensaysbak.ccplayerdata.model;
 
 public enum Rank {
-    NONE, EGG, CHICK, CHICKEN, ELDER, CLOUDIAN
+
+    NONE, EGG, CHICK, CHICKEN, ELDER, CLOUDIAN;
+
+    public static Rank fromString(String str) throws IllegalArgumentException {
+
+        if (str.matches("\\d+")) {
+
+            int ordinal = Integer.parseInt(str);
+            Rank[] values = Rank.values();
+
+            if (ordinal >= 0 && ordinal < values.length) return values[ordinal];
+
+        }
+
+        return Rank.valueOf(str.toUpperCase());
+
+    }
+
 }
