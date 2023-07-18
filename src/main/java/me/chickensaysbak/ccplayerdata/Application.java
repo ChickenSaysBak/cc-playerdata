01409870febec07ec02f5045494c841f4ee2379d
ChickenSaysBak <nicholash2000@icloud.com>
2023-07-17 22:46:01 -0400

    Added api documentation with swagger-ui

--- src/main/java/me/chickensaysbak/ccplayerdata/Application.java
+++ src/main/java/me/chickensaysbak/ccplayerdata/Application.java
@@ -1,13 +1,34 @@
 package me.chickensaysbak.ccplayerdata;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+
+		Contact contact = new Contact()
+				.name("CozyCloud")
+				.url("https://discord.gg/vBEWAuY");
+
+		Info info = new Info()
+				.title("CozyCloud Playerdata API")
+				.description("API for retrieving CozyCloud playerdata.")
+				.version("1.0.0")
+				.contact(contact);
+
+		return new OpenAPI().info(info);
+
 	}
 
 }
--- src/main/java/me/chickensaysbak/ccplayerdata/controller/PlayerdataController.java
+++ src/main/java/me/chickensaysbak/ccplayerdata/controller/PlayerdataController.java
@@ -1,5 +1,8 @@
 package me.chickensaysbak.ccplayerdata.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import me.chickensaysbak.ccplayerdata.model.Playerdata;
 import me.chickensaysbak.ccplayerdata.model.Rank;
 import me.chickensaysbak.ccplayerdata.repository.PlayerdataRepository;
@@ -13,6 +16,7 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/api/playerdata")
 @CrossOrigin
+@Tag(name = "Playerdata Endpoints")
 public class PlayerdataController {
 
     private final PlayerdataRepository repository;
@@ -22,42 +26,51 @@ public class PlayerdataController {
     }
 
     @GetMapping
+    @Operation(description = "Retrieves every player that has ever joined CozyCloud.")
     public List<Playerdata> findAll(@RequestParam(required = false, defaultValue = "0") Integer limit) {
         return repository.findAll(getPageRequest(limit));
     }
 
     @GetMapping("/first_joined")
+    @Operation(description = "Retrieves every player ordered by who joined first.")
     public List<Playerdata> findByFirstJoined(@RequestParam(required = false, defaultValue = "0") Integer limit) {
         return repository.findAllByOrderByFirstPlayed(getPageRequest(limit));
     }
 
     @GetMapping("/last_joined")
+    @Operation(description = "Retrieves every player ordered by who joined last.")
     public List<Playerdata> findByLastJoined(@RequestParam(required = false, defaultValue = "0") Integer limit) {
         return repository.findAllByOrderByFirstPlayedDesc(getPageRequest(limit));
     }
 
     @GetMapping("/year/{year}")
+    @Operation(description = "Retrieves every player who joined during a certain year.")
     public List<Playerdata> findByYearJoined(@PathVariable Integer year, @RequestParam(required = false, defaultValue = "0") Integer limit) {
         return repository.findAllBetweenTimes(getMillisFromYear(year), getMillisFromYear(year+1), getPageRequest(limit));
     }
 
     @GetMapping("/most_time")
+    @Operation(description = "Retrieves every player ordered by who has the largest time span.")
     public List<Playerdata> findByMostTime(@RequestParam(required = false, defaultValue = "0") Integer limit) {
         return repository.findAllByMostTime(getPageRequest(limit));
     }
 
     @GetMapping("/least_time")
+    @Operation(description = "Retrieves every player ordered by who has the smallest time span.")
     public List<Playerdata> findByLeastTime(@RequestParam(required = false, defaultValue = "0") Integer limit) {
         return repository.findAllByLeastTime(getPageRequest(limit));
     }
 
     @GetMapping("/rank")
+    @Operation(description = "Retrieves every player who has a rank.")
     public List<Playerdata> findRanked(@RequestParam(required = false, defaultValue = "0") Integer limit) {
         return repository.findAllRanked(getPageRequest(limit));
     }
 
     @GetMapping("/rank/{rank}")
-    public List<Playerdata> findByRank(@PathVariable String rank, @RequestParam(required = false, defaultValue = "0") Integer limit) {
+    @Operation(description = "Retrieves every player who has a certain rank.")
+    public List<Playerdata> findByRank(@Parameter(description = "Can be the rank name or a number 0-5 with 0 representing no rank. " +
+            "Rank names are none, egg, chick, chicken, elder, and cloudian.") @PathVariable String rank, @RequestParam(required = false, defaultValue = "0") Integer limit) {
 
         Rank r;
 
@@ -72,21 +85,27 @@ public class PlayerdataController {
     }
 
     @GetMapping("/uuid/{uuid}")
+    @Operation(description = "Retrieves the player with the matching uuid.")
     public Playerdata findByUuid(@PathVariable UUID uuid) {
         return repository.findByUuid(uuid).orElseThrow(() -> new NoSuchElementException("That player was not found."));
     }
 
     @GetMapping("/username/{username}")
+    @Operation(description = "Retrieves any players with a matching username. " +
+            "This should almost never return more than 1 value, though it is technically possible.")
     public List<Playerdata> findByUsername(@PathVariable String username, @RequestParam(required = false, defaultValue = "0") Integer limit) {
         return repository.findByUsername(username, getPageRequest(limit));
     }
 
-    @GetMapping("/search/{search}")
-    public List<Playerdata> findByUsernameSearch(@PathVariable String search, @RequestParam(required = false, defaultValue = "0") Integer limit) {
-        return repository.findAllByUsernameContainingIgnoreCase(search, getPageRequest(limit));
+    @GetMapping("/search/{keyword}")
+    @Operation(description = "Retrieves any players with a username containing the keyword.")
+    public List<Playerdata> findByUsernameSearch(@PathVariable String keyword, @RequestParam(required = false, defaultValue = "0") Integer limit) {
+        return repository.findAllByUsernameContainingIgnoreCase(keyword, getPageRequest(limit));
     }
 
     @GetMapping("/overlap/{uuid}")
+    @Operation(description = "Retrieves any players with time spans overlapping the player specified by uuid. " +
+            "The resulting players are ordered by most similar time span.")
     public List<Playerdata> findByOverlap(@PathVariable UUID uuid, @RequestParam(required = false, defaultValue = "0") Integer limit) {
         return repository.findAllByOverlap(uuid, getPageRequest(limit));
     }
